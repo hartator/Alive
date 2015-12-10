14d94fe111750973fe20462c8dea6a04344a25f3
hartator <hartator@gmail.com>
2015-12-10 18:32:38 +0100

    Remove react-rails references

--- config/application.rb
+++ config/application.rb
@@ -28,9 +28,5 @@ module Alive
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.react.addons = true
-    config.react.jsx_transform_options = {
-      harmony: true
-    }
   end
 end
